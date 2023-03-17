e83658c44338fe1a913d870876646f0d9f0b0492
TweakPY <69265406+Filza2@users.noreply.github.com>
2023-03-17 18:14:52 +0300

    Update URL-Short.py

--- URL-Short.py
+++ URL-Short.py
@@ -40,15 +40,17 @@ def short_url_4():
     r=post('https://is.gd/create.php',headers={'Host': 'is.gd','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded'},data=f'url={url}&shorturl=&opt=0')
     try:short_url=re.findall('''<a id="qrlabel" onclick="load_qrcode('(.*?)'); return false" href="https://chart.apis.google.com/chart?cht=qr&amp;chs=100x100&amp;choe=UTF-8&amp;chld=H|0&amp;chl=(.*?)">Give me this URL as a QR code</a>''',r.text)[0][2]
     except:exit('- Error ! ')
-    print(f'- Done successfully, Your URL : {short_url}')
-
+    if 'https://is.gd/' in r.text:print(f'- Done successfully, Your URL : {short_url}')
+    else:exit('- Error ! ')
+        
 def short_url_5():
     header()
     url=input("- Enter The URL : ");header()
     r=post('https://da.gd/',headers={'Host': 'da.gd','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/111.0','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8','Accept-Language': 'ar,en-US;q=0.7,en;q=0.3','Accept-Encoding': 'gzip, deflate','Content-Type': 'application/x-www-form-urlencoded'},data=f'url={url}&shorturl=')
     try:short_url=re.findall('<a href="(.*?)" style="color: #888; font-family: monospace;">(.*?)</a>',r.text)[0][1]
     except:exit('- Error ! ')
-    print(f'- Done successfully, Your URL : {short_url}')
+    if 'https://da.gd/' in r.text:print(f'- Done successfully, Your URL : {short_url}')
+    else:exit('- Error ! ')
 
 
 def Core():
@@ -66,6 +68,8 @@ Enter ID : '''))
     elif _short==4:short_url_4()
     elif _short==5:short_url_5()
     else:exit("- It seems that this is not Right ? ")
+
+
 
 header();Core()
 print('\n')
